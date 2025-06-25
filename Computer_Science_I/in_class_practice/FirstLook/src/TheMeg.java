3b17689e9d9b3e6e5c3bb2c1f1cf931ae031844c
Tylar Gossett <156851308+Saucy-tgossett@users.noreply.github.com>
2025-06-25 11:26:44 -0400

    Delete Computer_Science_I directory

--- Computer_Science_I/in_class_practice/FirstLook/src/TheMeg.java
+++
@@ -1,8 +0,0 @@
-public class TheMeg {
-    //program writtin by Tylar Gossett
-    public static void main(String[] args) throws Exception {
-        System.out.println("The Meg");
-        System.out.println(" ");
-        System.out.println("Hello, Mom");
-    }//closing main
-}//closing class
